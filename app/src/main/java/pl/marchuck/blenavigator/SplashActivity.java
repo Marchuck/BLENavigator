3bb889aee633726558d97b9591ac00c4e81bf916
lukasz <lukmar993@gmail.com>
2016-07-11 17:51:05 +0200

    refactor

--- app/src/main/java/pl/marchuck/blenavigator/SplashActivity.java
+++ app/src/main/java/pl/marchuck/blenavigator/SplashActivity.java
@@ -1,44 +1,24 @@
 package pl.marchuck.blenavigator;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 
 import com.github.jorgecastillo.FillableLoader;
-import com.github.jorgecastillo.FillableLoaderBuilder;
-import com.github.jorgecastillo.clippingtransforms.PlainClippingTransform;
 
 import pl.marchuck.blenavigator.common.WeakHandler;
 
 public class SplashActivity extends AppCompatActivity {
 
-    FillableLoader fillableLoader;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        fillableLoader = (FillableLoader) findViewById(R.id.fillableLoader);
-//        FrameLayout rootView = (FrameLayout) findViewById(R.id.rootView);
+        initFillableLoader();
+    }
 
-//        FillableLoaderBuilder loaderBuilder = new FillableLoaderBuilder();
-//        fillableLoader = loaderBuilder
-//                .parentView((FrameLayout) rootView)
-//                .layoutParams(new RelativeLayout.LayoutParams(300, 300))
-//                .svgPath(Paths.GITHUB)
-//                .originalDimensions(970, 970)
-//                .strokeWidth(2)
-//                .strokeColor(Color.parseColor("#1c9ade"))
-//                .fillColor(Color.parseColor("#1c9ade"))
-//                .strokeDrawingDuration(2000)
-//                .fillDuration(5000)
-//                .clippingTransform(new PlainClippingTransform())
-//                .build();
-
+    private void initFillableLoader() {
+        FillableLoader fillableLoader = (FillableLoader) findViewById(R.id.fillableLoader);
         fillableLoader.setSvgPath(Paths.GITHUB);
         fillableLoader.start();
         new WeakHandler().postDelayed(new Runnable() {
@@ -47,8 +27,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 int in = android.R.anim.fade_in;
                 int out = android.R.anim.fade_out;
-//                overridePendingTransition(in, out);
-                overridePendingTransition(out, in);
+                overridePendingTransition(in, out);
                 finish();
             }
         }, 6000);
